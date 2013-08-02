a30d5748b4ab10f60f672433da1dfe6315161faf
katee <hello@kate.io>
2013-08-02 17:14:44 -0400

    Sprinkles in some sugarjs.

--- node/public/all.js
+++ node/public/all.js
@@ -1,9 +1,14 @@
 $(function() {
   var socket = io.connect();
+  var colorSliders = [$('#red'), $('#green'), $('#blue')];
 
   // update background of page with color from server
   socket.on('color', function (color) {
     $('.ui-page').css('background', color );
+    var colors = color.split(',').map(function(string){return Number(string.replace(/[^0-9]/g, ''));});
+    colors.zip(colorSliders).each(function(thing){
+      $(thing[1]).val(thing[0]).slider('refresh');
+    });
   });
 
   // when a slider changes
